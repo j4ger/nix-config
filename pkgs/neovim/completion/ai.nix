27b0b2763d506506eb59dd26bdc367e204a5ea30
j4ger <xiayuxuan@live.com>
2024-10-17 01:24:50 +0800

    update keymaps

--- pkgs/neovim/completion/ai.nix
+++ pkgs/neovim/completion/ai.nix
@@ -10,6 +10,14 @@
           auto_refresh = true;
         };
       };
+      suggestion.keymap = {
+        accept = "<C-a>";
+        acceptLine = "<C-i>";
+        acceptWord = "<C-o>";
+        dismiss = "<C-b>";
+        next = "<C-n>";
+        prev = "<C-p>";
+      };
     };
   };
   keymaps = [
@@ -21,45 +29,9 @@
     }
     {
         mode = "n";
-        key = "<C-a>";
-        action = ":lua require('copilot.panel').accept()<CR>";
-        options.desc = "Accept Copilot Edit";
-    }
-    {
-        mode = "n";
         key = "<leader>mt";
         action = ":lua require('copilot.suggestion').toggle_auto_trigger()<CR>";
         options.desc = "Toggle Copilot AutoTrigger";
-    }
-    {
-        mode = "n";
-        key = "<C-D>";
-        action = ":lua require('copilot.suggestion').dismiss()<CR>";
-        options.desc = "Dismiss Copilot";
-    }
-    {
-        mode = "n";
-        key = "<C-e>";
-        action = ":lua require('copilot.suggestion').accept_line()<CR>";
-        options.desc = "Accept Copilot Line Edit";
-    }
-    {
-        mode = "n";
-        key = "<C-i>";
-        action = ":lua require('copilot.suggestion').accept_word()<CR>";
-        options.desc = "Accept Copilot Word Edit";
-    }
-    {
-        mode = "n";
-        key = "<C-]>";
-        action = ":lua require('copilot.suggestion').next()<CR>";
-        options.desc = "Copilot Next Suggestion";
-    }
-    {
-        mode = "n";
-        key = "<C-[>";
-        action = ":lua require('copilot.suggestion').prev()<CR>";
-        options.desc = "Copilot Previous Suggestion";
     }
   ]; 
 }
