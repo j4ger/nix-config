1871cc80ea883d1d7d113881210856243dc33042
j4ger <xiayuxuan@live.com>
2025-01-27 14:02:51 +0800

    add packages

--- home-manager/terminal.nix
+++ home-manager/terminal.nix
@@ -10,7 +10,7 @@
     termscp
     proxychains-ng
     distrobox
-    feh
+    qimgv
   ];
   programs = {
     ghostty = {
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -60,23 +60,6 @@
       permittedInsecurePackages = [
         "openssl-1.1.1w"
       ];
-      packageOverrides = pkgs: {
-        steam = pkgs.steam.override {
-          extraPkgs = pkgs:
-            with pkgs; [
-              xorg.libXcursor
-              xorg.libXi
-              xorg.libXinerama
-              xorg.libXScrnSaver
-              libpng
-              libpulseaudio
-              libvorbis
-              stdenv.cc.cc.lib
-              libkrb5
-              keyutils
-            ];
-        };
-      };
     };
   };
 
