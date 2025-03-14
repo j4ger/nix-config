2145a14a307141e8032ab45ab906b515fa356891
j4ger <xiayuxuan@live.com>
2025-03-14 12:46:59 +0800

    Add wlogout program config and update flake.lock

--- home-manager/hyprland/default.nix
+++ home-manager/hyprland/default.nix
@@ -313,8 +313,6 @@ in {
     nwg-look
     swww_latest
 
-    wlogout
-
     playerctl
 
     pamixer
@@ -327,6 +325,42 @@ in {
 
     bibata-cursors
   ];
+
+  programs.wlogout = {
+    enable = true;
+    layout = [
+      {
+          "label" = "shutdown";
+          "action" = "systemctl poweroff";
+          "text" = "Shutdown";
+          "keybind" = "s";
+      }
+      {
+          "label" = "reboot";
+          "action" = "systemctl reboot";
+          "text" = "Reboot";
+          "keybind" = "r";
+      }
+      {
+          "label" = "logout";
+          "action" = "loginctl kill-session $XDG_SESSION_ID";
+          "text" = "Logout";
+          "keybind" = "e";
+      }
+      {
+          "label" = "hibernate";
+          "action" = "systemctl hibernate";
+          "text" = "Hibernate";
+          "keybind" = "h";
+      }
+      {
+          "label" = "lock";
+          "action" = "hyprlock";
+          "text" = "Lock";
+          "keybind" = "l";
+      }
+    ];
+  };
 
   xdg.configFile."hypr/hyprshade.toml".text = ''
     [[shades]]
