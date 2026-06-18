89eb45e5593485692b574cfe020b669b102814f1
j4ger <xiayuxuan@live.com>
2026-06-19 00:37:25 +0800

    update

--- home-manager/misc.nix
+++ home-manager/misc.nix
@@ -29,6 +29,35 @@
       scrcpy
       kdePackages.kdenlive
       orca-slicer
+	((pkgs.orca-slicer.override {
+	  withNvidiaGLWorkaround = true;
+	  eigen = pkgs.eigen.overrideAttrs (old: rec {
+	    version = "5.0.1";
+	    src = pkgs.fetchFromGitLab {
+	      owner = "libeigen";
+	      repo = "eigen";
+	      tag = version;
+	      hash = "sha256-8TW1MUXt2gWJmu5YbUWhdvzNBiJ/KIVwIRf2XuVZeqo=";
+	    };
+	    patches = [ ];
+	  });
+	  wxwidgets_3_1 =
+	    let wx33 = pkgs.wxwidgets_3_3;
+	    in wx33 // {
+	      override = _args: wx33.override { withWebKit = true; };
+	    };
+	}).overrideAttrs (old: rec {
+	  version = "2.4.0-beta";
+	  src = pkgs.fetchFromGitHub {
+	    owner = "OrcaSlicer";
+	    repo = "OrcaSlicer";
+	    tag = "v${version}";
+	    hash = "sha256-bx4faVtEkcqBXzSXBXIsntDA4EFxDxWyUeI583tYhdw=";
+	  };
+	  patches = builtins.filter
+	    (p: !(lib.hasInfix "7650" (toString p)))
+	    (old.patches or [ ]);
+	}))
       frp
       prismlauncher
     ];
