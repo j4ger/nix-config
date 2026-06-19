21dc90e329edb2116626da27585f3eb13d925cb8
j4ger <xiayuxuan@live.com>
2026-06-19 13:47:18 +0800

    fix orca slicer

--- home-manager/programs/misc.nix
+++ home-manager/programs/misc.nix
@@ -24,36 +24,12 @@
     zed-editor
     scrcpy
     kdePackages.kdenlive
-    orca-slicer
-    ((pkgs.orca-slicer.override {
+    (orca-slicer.override {
       withNvidiaGLWorkaround = true;
-      eigen = pkgs.eigen.overrideAttrs (old: rec {
-        version = "5.0.1";
-        src = pkgs.fetchFromGitLab {
-          owner = "libeigen";
-          repo = "eigen";
-          tag = version;
-          hash = "sha256-8TW1MUXt2gWJmu5YbUWhdvzNBiJ/KIVwIRf2XuVZeqo=";
-        };
-        patches = [ ];
+      glew = (glew.override {
+        enableEGL = false;
       });
-      wxwidgets_3_1 =
-        let wx33 = pkgs.wxwidgets_3_3;
-        in wx33 // {
-          override = _args: wx33.override { withWebKit = true; };
-        };
-    }).overrideAttrs (old: rec {
-      version = "2.4.0-beta";
-      src = pkgs.fetchFromGitHub {
-        owner = "OrcaSlicer";
-        repo = "OrcaSlicer";
-        tag = "v${version}";
-        hash = "sha256-bx4faVtEkcqBXzSXBXIsntDA4EFxDxWyUeI583tYhdw=";
-      };
-      patches = builtins.filter
-        (p: !(lib.hasInfix "7650" (toString p)))
-        (old.patches or [ ]);
-    }))
+    })
     
     # Networking/tools
     frp
